a622c90605ad7bdb9152536dd2ded0d35db4576d
bamamaulanaw <bamamaulanaw@gmail.com>
2026-01-11 08:15:29 +0700

    Add views

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+@extends('templates.layout')
+
+@section('title', 'About Page')
+
+@section('content')
+<div class="card">
+  <div class="card-header">
+    <h3>Tentang Aplikasi</h3>
+  </div>
+  <div class="card-body">
+    <p>Aplikasi POS ini dibuat pada tahun 2026 untuk mengelola data penjualan.</p>
+  </div>
+</div>
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,19 @@
+@extends('templates.layout')
+
+@section('title', 'Contact Page')
+
+@section('content')
+<div class="card">
+  <div class="card-header">
+    <h3>Kontak</h3>
+  </div>
+  <div class="card-body">
+    <p>Hubungi kami:</p>
+    <ul>
+      <li>Email: admin@pos.com</li>
+      <li>Telepon: +62 812-3456-7890</li>
+      <li>Alamat: Jl. Sukses No. 123</li>
+    </ul>
+  </div>
+</div>
+@endsection
